d2d91cb22f8eda756e93b75a9513c0b1ee4f17ac
Tristan | Tribot17 <tribot17@gmail.com>
2023-04-13 01:40:49 +0200

    blockchain data exercice

---
+++ ExerciceX.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.8.19;
+import "./IERC20.sol";
+
+
+contract ExerciceX{
+    IERC20 token;
+
+    mapping(address => bool) userValided;
+
+    constructor(address _tokenAddress) {
+        token = IERC20(_tokenAddress);
+    }
+
+    function userHasValided(address user) public view returns (bool) {
+        return userValided[user];
+    }
+
+    function claimToken(bytes32 _blockhash, uint _blockNumer, uint _chainId) public {
+        require(userValided[msg.sender] == false, "you have already claim");
+        require(_blockNumer >= block.number + 10, "The block is not the range");
+        require(blockhash(_blockNumer) == _blockhash, "Wrong blockhash");
+        require(_chainId == block.chainid, "Wrong chain Id");
+
+        userValided[msg.sender] = true;
+        token.transfer(msg.sender, 1 ether);
+    }
+}
